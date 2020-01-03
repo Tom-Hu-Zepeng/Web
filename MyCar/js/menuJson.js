0d252e7e448293eb9ac339b3bc1cf3b0e25b3664
Tom <soul960918@163.com>
2020-01-03 14:35:04 +0800

    第一次提交

---
+++ MyCar/js/menuJson.js
@@ -0,0 +1,206 @@
+/*leftNav_id不可重复、以下属性名不可更改，否则页面要进行相应调整*/
+var leftNav = [{
+	"text": "首页",
+	"leftNav_id": "1",
+	"name": "个人中心",
+	"href": "html/employees.html"
+},{
+	"text": "首页",
+	"leftNav_id": "1",
+	"name": "修改密码",
+	"href": "html/login/layui/login.html"
+},{
+	"text": "首页",
+	"leftNav_id": "1",
+	"name": "注销账户",
+	"href": "html/login/layui/login.html"
+},{
+	"text": "前台结算",
+	"leftNav_id": "24",
+	"name": "课程管理",
+	"href": "html/kecheng/index.html"
+}, {
+	"text": "前台结算",
+	"leftNav_id": "26",
+	"name": "消息管理",
+	"href": "html/messageList.html"
+}, {
+	"text": "收益统计",
+	"leftNav_id": "3",
+	"name": "活动管理",
+	"href": "html/activity.html"
+}, {
+	"text": "业绩考核",
+	"leftNav_id": "4",
+	"name": "项目列表",
+	"href": "html/ItemsSys/index.html"
+}, {
+	"text": "卡管理",
+
+	"leftNav_id": "5",
+	"name": "项目团队",
+	"href": "html/ItemsSys/projectteam.html"
+}, {
+	"text": "其他统计",
+	"leftNav_id": "23",
+	"name": "项目检查",
+	"href": "html/ItemsSys/projectcheck.html"
+}]
+
+var letNavq = [{
+	"text": "首页",
+	"leftNav_id": "1",
+	"name": "个人中心",
+	"href": "html/employees.html"
+},{
+	"text": "首页",
+	"leftNav_id": "1",
+	"name": "修改密码",
+	"href": "html/login/layui/login.html"
+},{
+	"text": "首页",
+	"leftNav_id": "1",
+	"name": "注销账户",
+	"href": "html/login/layui/login.html"
+},{
+	"text": "XXX1",
+	"leftNav_id": "24",
+	"name": "XXX1",
+	"href": "html/kecheng/index.html"
+}, {
+	"text": "XXX1",
+	"leftNav_id": "26",
+	"name": "XXX1",
+	"href": "html/messageList.html"
+}, {
+	"text": "XXX2",
+	"leftNav_id": "3",
+	"name": "XXX2",
+	"href": "html/activity.html"
+}, {
+	"text": "XXX3",
+	"leftNav_id": "4",
+	"name": "XXX3",
+	"href": "html/ItemsSys/index.html"
+}, {
+	"text": "XXX4",
+	"leftNav_id": "5",
+	"name": "XXX4",
+	"href": "html/ItemsSys/projectteam.html"
+}, {
+	"text": "XXX4",
+	"leftNav_id": "23",
+	"name": "XXX4",
+	"href": "html/ItemsSys/projectcheck.html"
+}]
+
+var letNavq1 = [{
+	"text": "首页",
+	"leftNav_id": "1",
+	"name": "个人中心",
+	"href": "html/employees.html"
+},{
+	"text": "首页",
+	"leftNav_id": "1",
+	"name": "修改密码",
+	"href": "html/login/layui/login.html"
+},{
+	"text": "首页",
+	"leftNav_id": "1",
+	"name": "注销账户",
+	"href": "html/login/layui/login.html"
+},{
+	"text": "XXX1",
+	"leftNav_id": "24",
+	"name": "XXX1",
+	"href": "html/kecheng/index.html"
+}, {
+	"text": "XXX1",
+	"leftNav_id": "26",
+	"name": "XXX1",
+	"href": "html/messageList.html"
+}, {
+	"text": "XXX2",
+	"leftNav_id": "3",
+	"name": "XXX2",
+	"href": "html/activity.html"
+}, {
+	"text": "XXX3",
+	"leftNav_id": "4",
+	"name": "XXX3",
+	"href": "html/ItemsSys/index.html"
+}, {
+	"text": "XXX4",
+	"leftNav_id": "5",
+	"name": "XXX4",
+	"href": "html/ItemsSys/projectteam.html"
+}, {
+	"text": "XXX4",
+	"leftNav_id": "23",
+	"name": "XXX4",
+	"href": "html/ItemsSys/projectcheck.html"
+}]
+
+var letNavq2 = [{
+	"text": "opopopo",
+	"leftNav_id": "1",
+	"name": "kkkkkk8",
+	"href": "html/employees.html"
+},{
+	"text": "opopopo1",
+	"leftNav_id": "1",
+	"name": "kkkkkk7",
+	"href": "html/login/layui/login.html"
+},{
+	"text": "opopopo2",
+	"leftNav_id": "1",
+	"name": "kkkkkk6",
+	"href": "html/login/layui/login.html"
+},{
+	"text": "opopopo3",
+	"leftNav_id": "24",
+	"name": "kkkkkk5",
+	"href": "html/kecheng/index.html"
+}, {
+	"text": "opopopo4",
+	"leftNav_id": "26",
+	"name": "kkkkkk4",
+	"href": "html/messageList.html"
+}, {
+	"text": "opopopo5",
+	"leftNav_id": "3",
+	"name": "kkkkkk3",
+	"href": "html/activity.html"
+}]
+
+var letNavq3 = [{
+	"text": "kkkkkk8",
+	"leftNav_id": "1",
+	"name": "kkkkkk8",
+	"href": "html/employees.html"
+},{
+	"text": "kkkkkk7",
+	"leftNav_id": "1",
+	"name": "kkkkkk7",
+	"href": "html/login/layui/login.html"
+},{
+	"text": "kkkkkk6",
+	"leftNav_id": "1",
+	"name": "kkkkkk6",
+	"href": "html/login/layui/login.html"
+},{
+	"text": "kkkkkk5",
+	"leftNav_id": "24",
+	"name": "kkkkkk5",
+	"href": "html/kecheng/index.html"
+}, {
+	"text": "kkkkkk4",
+	"leftNav_id": "26",
+	"name": "kkkkkk4",
+	"href": "html/messageList.html"
+}, {
+	"text": "kkkkkk3",
+	"leftNav_id": "3",
+	"name": "kkkkkk3",
+	"href": "html/activity.html"
+}]
